c81e4b75852d426faad3d9f500f27d283d9306c6
Scott Lowe <scott.code.lowe@gmail.com>
2023-08-22 14:19:31 -0400

    RF: Import DistributedSampler directly to save long module path

--- template_experiment/train.py
+++ template_experiment/train.py
@@ -12,6 +12,7 @@ from datetime import datetime
 import torch
 import torch.optim
 from torch import nn
+from torch.utils.data.distributed import DistributedSampler
 
 from template_experiment import data_transformations, datasets, encoders, utils
 from template_experiment.evaluation import evaluate
@@ -357,17 +358,11 @@ def run_one_worker(gpu, ngpus_per_node, config):
 
     if config.distributed:
         # The DistributedSampler breaks up the dataset across the GPUs
-        dl_train_kwargs["sampler"] = torch.utils.data.distributed.DistributedSampler(
-            dataset_train
-        )
+        dl_train_kwargs["sampler"] = DistributedSampler(dataset_train)
         dl_train_kwargs["shuffle"] = None
-        dl_val_kwargs["sampler"] = torch.utils.data.distributed.DistributedSampler(
-            dataset_val
-        )
+        dl_val_kwargs["sampler"] = DistributedSampler(dataset_val)
         dl_val_kwargs["shuffle"] = None
-        dl_test_kwargs["sampler"] = torch.utils.data.distributed.DistributedSampler(
-            dataset_test
-        )
+        dl_test_kwargs["sampler"] = DistributedSampler(dataset_test)
         dl_test_kwargs["shuffle"] = None
 
     dataloader_train = torch.utils.data.DataLoader(dataset_train, **dl_train_kwargs)
@@ -761,9 +756,7 @@ def run_one_worker(gpu, ngpus_per_node, config):
     dl_train_eval_kwargs = copy.deepcopy(dl_test_kwargs)
     if config.distributed:
         # The DistributedSampler breaks up the dataset across the GPUs
-        dl_train_eval_kwargs[
-            "sampler"
-        ] = torch.utils.data.distributed.DistributedSampler(dataset_train_eval)
+        dl_train_eval_kwargs["sampler"] = DistributedSampler(dataset_train_eval)
         dl_train_eval_kwargs["shuffle"] = None
     dataloader_train_eval = torch.utils.data.DataLoader(
         dataset_train_eval, **dl_train_eval_kwargs
